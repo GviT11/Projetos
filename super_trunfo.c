42770505d863a8aacb9fda27131f8b1bb44e191f
GviT11 <gabriel7bh@gmail.com>
2025-03-06 14:09:47 +0000

    Atualização Final

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,10 +2,10 @@
 
 int main () {
 
-    char estado;
-    char cidade [20];
-    int pop, pt, codcarta; //pop = população, pt = ponto turistico, codcarta = codigo da carta//
-    float area, pib;
+    char estado, estado1;
+    char cidade [20], cidade1 [20];
+    int pop, pop1, pt, pt1, codcarta, codcarta1; //pop = população, pt = ponto turistico, codcarta = codigo da carta//
+    float area, area1, pib, pib1;
 
     printf ("Entre com os valores da Carta 1\n");
 printf ("Digite a letra representada pelo estado: \n");
@@ -29,14 +29,45 @@ scanf ("%f", &pib);
 printf ("Digite o numero de pontos turisticos dessa cidade: \n");
 scanf ("%i", &pt);
 
-printf ("Carta 1\n");
-printf ("Estado: %c\n", estado);
-printf ("Codigo: %c%i\n", estado, codcarta);
-printf ("Cidade: %s\n", cidade);
-printf ("População: %i\n", pop);
-printf ("Area: %f KM²\n", area);
-printf ("PIB: %f\n", pib);
-printf ("Pontos Turisticos: %i\n", pt);
+    printf ("Carta 1\n");
+    printf ("Estado: %c\n", estado);
+    printf ("Codigo: %c%i\n", estado, codcarta);
+    printf ("Cidade: %s\n", cidade);
+    printf ("População: %i\n", pop);
+    printf ("Area: %f KM²\n", area);
+    printf ("PIB: %f\n", pib);
+    printf ("Pontos Turisticos: %i\n", pt);
+
+    printf ("Entre com os valores da Carta 2\n");
+printf ("Digite a letra representada pelo estado: \n");
+scanf (" %c", &estado1);
+
+printf ("Digite o codigo da carta: \n ");
+scanf (" %i", &codcarta1);
+
+printf ("Digite o nome da cidade: \n");
+scanf (" %s", &cidade1);
+
+printf ("Digite o numero de habitantes da cidade: \n");
+scanf (" %i", &pop1);
+
+printf ("Digite a area da cidade em KM²: \n");
+scanf (" %f", &area1);
+
+printf ("Digite o PIB dessa cidade: \n");
+scanf (" %f", &pib1);
+
+printf ("Digite o numero de pontos turisticos dessa cidade: \n");
+scanf (" %i", &pt1);
+
+    printf ("Carta 2\n");
+    printf ("Estado: %c\n", estado1);
+    printf ("Codigo: %c%i\n", estado1, codcarta1);
+    printf ("Cidade: %s\n", cidade1);
+    printf ("População: %i\n", pop1);
+    printf ("Area: %f KM²\n", area1);
+    printf ("PIB: %f\n", pib1);
+    printf ("Pontos Turisticos: %i\n", pt1);
 
 return 0;
 
